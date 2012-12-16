131d676ecaa6e790bbfd3bf854e22f67d53c27a8
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2012-12-16 15:54:23 +0400

    More descriptive siphash24() signature
    Signed-off-by: Gregory Petrosyan <gregory.petrosyan@gmail.com>

--- siphash.h
+++ siphash.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 
 
-void siphash24(uint8_t const* key, void const* data, size_t size, uint8_t* out);
+void siphash24(uint8_t const* key16, void const* data, size_t size, uint8_t* out8);
 
 
 #endif
